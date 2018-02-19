c73d1ddfc7856a9ac88db6b7aad0f3ab2fcc0853
souptik-ionic <souptik89@gmail.com>
2018-02-20 00:54:11 +0530

    updated by souptik

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PaymentWithOptionPage } from '../payment-with-option/payment-with-option';
+import { CardpayPage } from '../cardpay/cardpay';
 /**
  * Generated class for the PaymentPage page.
  *
@@ -14,14 +15,34 @@ import { PaymentWithOptionPage } from '../payment-with-option/payment-with-optio
   templateUrl: 'payment.html',
 })
 export class PaymentPage {
-
+showoptions:boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.showoptions=false;
   }
+
+  items = [{
+    title: 'tewst',
+    headimageUrl: 'assets/img/payment-logo_1.png'
+  },{
+    title: 'tewst',
+    headimageUrl: 'assets/img/payment-logo_1.png'
+  },{
+    title: 'tewst',
+    headimageUrl: 'assets/img/payment-logo_1.png'
+  },{
+    title: 'tewst',
+    headimageUrl: 'assets/img/payment-logo_1.png'
+  }
+  ];
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PaymentPage');
   }
   options() {
-    this.navCtrl.push(PaymentWithOptionPage);
+    this.showoptions=true;
+  }
+  
+  cardpay() {
+    this.navCtrl.push(CardpayPage);
   }
 }
